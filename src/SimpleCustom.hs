cc8a86fc62188c00c8458fb834270d116a567091
Sean Hess <seanhess@gmail.com>
2023-08-22 10:40:14 -0600

    added context

--- src/SimpleCustom.hs
+++ src/SimpleCustom.hs
@@ -4,23 +4,33 @@ module SimpleCustom where
 
 import Control.Monad.Identity (Identity (..))
 import Control.Monad.Writer.Strict
-import Data.Kind
+
+-- import Data.Kind
 import Data.Proxy
 
-newtype Custom a = Custom {runCustom :: IO a}
-  deriving (Functor, Applicative, Monad)
+newtype Custom r a = Custom {runCustom :: r -> IO a}
+  deriving (Functor)
+
+instance Applicative (Custom r) where
+  pure a = Custom (\_ -> pure a)
+  mf <*> ma = Custom $ \r -> do
+    f <- runCustom mf r
+    a <- runCustom ma r
+    pure (f a)
+
+instance Monad (Custom r) where
+  ma >>= fmb = Custom $ \r -> do
+    a <- runCustom ma r
+    runCustom (fmb a) r
 
 newtype Network a = Network {runNetwork :: Writer [(String, String)] a}
   deriving (Functor, Applicative, Monad, MonadWriter [(String, String)])
-
--- TODO: custom monad
--- perform tasks
--- fetch dependencies
 
 data A = A deriving (Show)
 data B = B deriving (Show)
 data C = C deriving (Show)
 data D = D deriving (Show)
+data Dataset = Dataset deriving (Show)
 
 class Node a where
   nodeName :: Proxy a -> [String]
@@ -43,7 +53,7 @@ instance Node () where
 instance (Node a, Node b) => Node (a, b) where
   nodeName _ = mconcat [nodeName @a Proxy, nodeName @b Proxy]
 
-workflow :: forall m f. (Flow m f) => m (f ())
+workflow :: Flow Dataset m => m ()
 workflow = do
   ta <- task0 runA
   tb <- task1 runB ta
@@ -51,62 +61,63 @@ workflow = do
   td <- task2 runD tb tc
   task2 runEnd ta td
 
-task0 :: (Node a, Flow m f) => IO a -> m (f a)
-task0 t = task (const t) (pure ())
+task0 :: (Node a, Flow r m) => (r -> IO a) -> m a
+task0 t = task (const t) ()
 
-task1 :: (Node a, Node i, Flow m f) => (i -> IO a) -> (f i -> m (f a))
+task1 :: (Node a, Node i, Flow r m) => (i -> r -> IO a) -> (i -> m a)
 task1 = task
 
-task2 :: (Node a, Node i, Node v, Flow m f) => (i -> v -> IO a) -> f i -> f v -> m (f a)
-task2 t fi fv = task (uncurry t) $ (,) <$> fi <*> fv
+task2 :: (Node a, Node i, Node v, Flow r m) => (i -> v -> r -> IO a) -> i -> v -> m a
+task2 t fi fv = task (uncurry t) (fi, fv)
 
-class (Monad m, Applicative f) => Flow m f where
-  task :: (Node a, Node i) => (i -> IO a) -> (f i -> m (f a))
+class (Monad m) => Flow r m where
+  task :: (Node a, Node i) => (i -> r -> IO a) -> (i -> m a)
 
-instance Flow Network Proxy where
-  task :: forall a i. (Node a, Node i) => (i -> IO a) -> (Proxy i -> Network (Proxy a))
+instance Flow Dataset Network where
+  task :: forall a i. (Node a, Node i) => (i -> Dataset -> IO a) -> (i -> Network a)
   task _ _ = do
     let adp = nodeName @a Proxy
     let idp = nodeName @i Proxy
     tell $ [(ni, na) | na <- adp, ni <- idp]
-    pure Proxy
 
-instance Flow IO Identity where
-  task :: forall a i. (Node a, Node i) => (i -> IO a) -> (Identity i -> IO (Identity a))
-  task t (Identity i) = do
-    a <- t i
-    pure $ Identity a
+    -- WARNING: use undefined instead of requiring Flow to return a functor
+    -- if necessary we could use defaults instead
+    pure $ error "Network should not evaluate values"
 
-type family Input a :: Type
-type instance Input (IO a) = ()
-type instance Input (i -> IO a) = i
-type instance Input (i1 -> i2 -> IO a) = (i1, i2)
+-- pure Proxy
 
--- The tasks are sane!
-runA :: IO A
-runA = pure A
+instance Flow Dataset (Custom Dataset) where
+  -- TODO: exception catching
+  -- TODO: caching
+  task :: forall a i. (Node a, Node i) => (i -> Dataset -> IO a) -> (i -> Custom Dataset a)
+  task t i = Custom $ t i
 
-runB :: A -> IO B
-runB _ = pure B
+-- TODO: Task type, instead of IO?
+runA :: Dataset -> IO A
+runA _ = pure A
 
-runC :: A -> IO C
-runC _ = pure C
+runB :: A -> Dataset -> IO B
+runB _ _ = pure B
 
-runD :: B -> C -> IO D
-runD _ _ = pure D
+runC :: A -> Dataset -> IO C
+runC _ _ = pure C
 
-runEnd :: A -> D -> IO ()
-runEnd a d = do
+runD :: B -> C -> Dataset -> IO D
+runD _ _ _ = pure D
+
+runEnd :: A -> D -> Dataset -> IO ()
+runEnd a d ds = do
   print a
   print d
+  print ds
   pure ()
 
 test :: IO ()
 test = do
-  let network = workflow :: Network (Proxy ())
+  let network = workflow :: Network ()
       deps = execWriter $ runNetwork network
 
   print deps
 
-  _ <- workflow :: IO (Identity ())
+  _ <- runCustom (workflow :: Custom Dataset ()) Dataset
   pure ()
